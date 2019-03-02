1849f3f7d13f57e482d2d59fb514c9a57bbac189
Gabriel Mukobi <gabrielmukobi@gmail.com>
2019-03-01 17:19:12 -0800

    1.0 Ball therapy up and down

---
+++ Ball Therapy VR/Assets/BallScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(gameObject.GetComponentInParent<PathScript>().objectToTrigger.tag == other.tag)
+        {
+            gameObject.GetComponentInParent<PathScript>().advancePath(gameObject);
+        }
+    }
+}
---
+++ Ball Therapy VR/Assets/TranslationFollower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslationFollower : MonoBehaviour
+{
+    public GameObject toFollow;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Debug.Log("Follower collided with: " + collision.gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = toFollow.transform.position;
+    }
+}
